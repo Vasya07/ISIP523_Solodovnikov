67e41cc2f2952be0a8eeed35b0ea4a4f8f13c056
Vasya <quartz880@yandex.ru>
2025-10-14 14:46:52 +0300

    Создаём класс всех предметов в игре (класс Item) и создаём классы Weapon и Armor с наследованием от класса Item

--- ISIP523_Solodovnikov/Program.cs
+++ ISIP523_Solodovnikov/Program.cs
@@ -1,1 +1,43 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace TextRoguelike
+{
+    public abstract class Item
+    {
+        public string Name { get; protected set; }
+
+        public Item(string name)
+        {
+            Name = name;
+        }
+
+        public abstract void DisplayStats();
+    }
+
+    public class Weapon : Item
+    {
+        public int Attack { get; private set; }
+
+        public Weapon(string name, int attack) : base(name)
+        {
+            Attack = attack;
+        }
+
+        public override void DisplayStats()
+        {
+            Console.WriteLine($"{Name} (Атака: {Attack})");
+        }
+    }
+
+    public class Armor : Item
+    {
+        public int Defense { get; private set; }
+
+        public Armor(string name, int defense) : base(name)
+        {
+            Defense = defense;
+        }
+
+        public override void DisplayStats()
+        {
+            Console.WriteLine($"{Name} (Защита: {Defense})");
+        }
+    }
