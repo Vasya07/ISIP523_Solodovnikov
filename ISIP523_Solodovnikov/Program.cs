6e267e60144e7962f80fd2f68bbfcf2400a902b9
Vasya <quartz880@yandex.ru>
2025-10-14 14:49:25 +0300

    Создаём класс для всех существ (класс Creature) и создаём класс Enemy с наследованием от класса Creature

--- ISIP523_Solodovnikov/Program.cs
+++ ISIP523_Solodovnikov/Program.cs
@@ -41,3 +41,51 @@
             Console.WriteLine($"{Name} (Защита: {Defense})");
         }
     }
+    public abstract class Creature
+    {
+        public string Name { get; protected set; }
+        public int MaxHP { get; protected set; }
+        public int CurrentHP { get; protected set; }
+        public int Attack { get; protected set; }
+        public int Defense { get; protected set; }
+
+        public bool IsAlive => CurrentHP > 0;
+
+        protected Creature(string name, int maxHP, int attack, int defense)
+        {
+            Name = name;
+            MaxHP = maxHP;
+            CurrentHP = maxHP;
+            Attack = attack;
+            Defense = defense;
+        }
+
+        public virtual void TakeDamage(int damage)
+        {
+            CurrentHP -= damage;
+            if (CurrentHP < 0) CurrentHP = 0;
+        }
+
+        public void Heal(int amount)
+        {
+            CurrentHP += amount;
+            if (CurrentHP > MaxHP) CurrentHP = MaxHP;
+        }
+
+        public virtual void DisplayStats()
+        {
+            Console.WriteLine($"{Name} - HP: {CurrentHP}/{MaxHP}, Атака: {Attack}, Защита: {Defense}");
+        }
+    }
+    public abstract class Enemy : Creature
+    {
+        protected Random random;
+
+        public Enemy(string name, int maxHP, int attack, int defense) : base(name, maxHP, attack, defense)
+        {
+            random = new Random();
+        }
+
+        public abstract int CalculateDamage(int playerDefense);
+        public abstract string GetSpecialAbility();
+    }
