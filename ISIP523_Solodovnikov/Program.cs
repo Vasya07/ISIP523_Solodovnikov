fd27a13fa4efd5a5d3c8ca18f838ba446799594b
Vasya <quartz880@yandex.ru>
2025-10-14 14:56:38 +0300

    Добавляем методы ко всем боссами

--- ISIP523_Solodovnikov/Program.cs
+++ ISIP523_Solodovnikov/Program.cs
@@ -199,3 +199,39 @@
             Attack = (int)(Attack * 1.6);
             Defense = (int)(Defense * 1.1);
         }
+
+        public new bool TryFreeze()
+        {
+            return random.NextDouble() < 0.35;
+        }
+    }
+
+    public class PestovC : Skeleton
+    {
+        private const double FREEZE_CHANCE = 0.4;
+
+        public PestovC() : base()
+        {
+            Name = "Пестов С-- (Босс Скелет)";
+            MaxHP = (int)(MaxHP * 1.3);
+            CurrentHP = MaxHP;
+            Attack = (int)(Attack * 1.8);
+            Defense = (int)(Defense * 0.6);
+        }
+
+        public override int CalculateDamage(int playerDefense)
+        {
+            Console.WriteLine("Пестов С-- игнорирует вашу защиту!");
+            return Attack;
+        }
+
+        public bool TryFreeze()
+        {
+            return random.NextDouble() < FREEZE_CHANCE;
+        }
+
+        public override string GetSpecialAbility()
+        {
+            return "Игнорирует защиту + шанс заморозки (40%)";
+        }
+    }
